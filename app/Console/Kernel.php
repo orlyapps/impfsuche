7d6fd003488ef9bc8c334657810606354b49ed9b
Florian Strauß <fs@orlyapps.de>
2021-06-23 15:02:47 +0200

    “wip”

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('fetch "Bad Zwischenahn"')->everyFiveMinutes();
+        $schedule->command('fetch "Bad Zwischenahn"')->everyMinute();
     }
 
     /**
